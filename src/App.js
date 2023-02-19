e679d182faaeda18d16b015ff6ac70891a8cff44
avanthi9475 <avanthi9475@gmail.com>
2023-02-19 18:38:20 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import SignUp from "./pages/SignUp"
 import ForgotPassword from "./pages/ForgotPassword"
 import Profile from "./pages/Profile"
 import Offers from "./pages/Offers"
+import Header from "./components/Header";
 
 function App() {
   return (
       <>
         <Router>
+          <Header/>
           <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/profile" element={<Profile />}/>
